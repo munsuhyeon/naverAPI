7415b9d30c539e71e673669158a57fbe964f175b
munsuhyeon <hrno3826@naver.com>
2022-02-10 14:26:19 +0900

    api test code

--- naverAPICall.py
+++ naverAPICall.py
@@ -1,44 +1,78 @@
 import urllib.request
 import json
-
-import node as node
+import datetime
+import time
 
 client_id = 'YzE6dWuLaCuPh7j7dbP9'
 client_secret = 'zJUqCoO4ZD'
 
 def getRequestUrl(url):
-    req = urllib.request(url) # 네이버서버에 보낼 요청객체를 생성
-    req.add_header("X-Naver-Client-ID",client_id) # 위에서 만들어진 요청객체에 Client_id를 포함시킴
-    req.add_header("X-Naver-Client-Secret", client_secret) # 위에서 만들어진 요청객체에 Client_Secret를 포함시킴
+    req = urllib.request.Request(url) # 네이버서버에 보낼 요청객체를 생성
+    req.add_header("X-Naver-Client-Id",client_id) # 위에서 만들어진 요청객체에 client_id를 포함시킴
+    req.add_header("X-Naver-Client-Secret", client_secret)
 
-    response = urllib.request.urlopen(req) # 네이버서버에 요청객체 req를 전달하여 응답을 받아 response에 저장
-    if response.getcode() == 200: # 응답코드가 200이면 정상호출
-        print('호출성공!!')
-        ret = response.read().decode('utf-8')
-        return ret
-    else:
-        print('호출에러-호출에러코드:',response.getcode())
-        print('에러발생 주소:',url)
+    try:
+        response = urllib.request.urlopen(req) # 네이버서버에 요청객체 req를 전달하여 응답을 받아 response에 저장
+        if response.getcode() == 200: # 응답코드가 200이면 정상 호출
+            print('호출성공!!')
+            ret = response.read().decode('utf-8')
+            return ret
+    except:
+        # print('호출에러-호출에러코드:', response.getcode())
+        # print('에러발생 주소:', url)
+        print('더이상 가져올 데이터가 없거나 호출에 에러가 발생했습니다')
+
         return None
 
 def getNaverSearch(node, srcText, start, display):
-    baseUrl = "http://openapi.naver.com/v1/search"   # 네이버 기본 api주소
+    baseUrl = "https://openapi.naver.com/v1/search" # 네이버 기본 api 주소
     node = "/%s.json" % node
-    param = "?query=%s" % srcText
-    api_url = baseUrl + node + param # http://openapi.naver.com/v1/search/news.json?query='BTS'
-    responseDecode = getRequestUrl(api_url) #호출성공시 디코딩된 응답 데이터를 저장
+    param = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), start, display)
+    api_url = baseUrl + node + param # https://openapi.naver.com/v1/search/news.json?query='BTS'
+    responseDecode = getRequestUrl(api_url)  # 호출성공시 디코딩된 응답 데이터를 저장
+    if responseDecode == None:
+        return None
+    else:
+        return json.loads(responseDecode)
+
+def getPostData(post, jsonResult):
+    title = post['title'] # 기사제목 추출
+    description = post['description'] # 기사요약 추출
+    org_link = post['originallink'] # 기사 오리지널 링크(신문사링크) 추출
+    link = post['link'] # 네이버 기사 링크 추출
+    pubDate = datetime.datetime.strptime(post['pubDate'], '%a, %d %b %Y %H:%M:%S +0900') # 'Thu, 10 Feb 2022 09:37:00 +0900' 기사 게시일
+    pubDate = pubDate.strftime('%Y-%m-%d %H:%M:%S') # 2022-02-10 10:14:11 시간형식 변환
+
+    jsonResult.append({'title':title, 'description':description, 'org_link':org_link, 'link':link, 'pDate':pubDate})
 
 def main():
     node = 'news' # 검색카테고리를 news로 설정
-    srcText = input('원하시는 검색어를 입력하세요:')
-    jsonResponse = getNaverSearch(node, srcText, 1, 100) # news카테고리에서 입력된 검색어가 들어간 뉴스를 1~100개 추출하여 응답
+    jsonResult=[]
+    srcText = input('원하시는 검색어를 입력하세요:') # 검색어를 입력 받음
+    jsonResponse = getNaverSearch(node, srcText, 1, 100) # news 카테고리에서 입력된 검색어가 들어간 뉴스를 1~100개 추출하여 응답
+    print(jsonResponse['total'])
+    total = jsonResponse['total']
 
-    for post in jsonResponse: # 응답된 json에서 기사를 추출
-        pass
+    count = 0
 
-    with open('파일이름','w', encoding='UTF-8') as outfile:
-        jsonFile = json.dumps()
-        outfile.write(jsonFile)
+    while((jsonResponse !=None) and (jsonResponse['display'] != 0)):
+        for post in jsonResponse['items']: # 응답된 json에서 기사를 추출
+            count = count + 1
+            getPostData(post, jsonResult)
+
+        start = jsonResponse['start'] + jsonResponse['display']
+        jsonResponse = getNaverSearch(node, srcText, start, 100)
+
+    try:
+        with open('%s_네이버_%s.json' % (srcText, node), 'w', encoding='utf-8') as outfile:
+            jsonFile = json.dumps(jsonResult, indent=4, sort_keys=True, ensure_ascii=False)
+            outfile.write(jsonFile)
+        print('파일 출력에 성공하였습니다.')
+    except:
+        print('파일출력 에러가 발생하였습니다.')
+
+    print('전체 검색 기사 건수 : ', total)
+    print('전체 출력건수 : ', count)
 
 if __name__ == '__main__':
     main()
